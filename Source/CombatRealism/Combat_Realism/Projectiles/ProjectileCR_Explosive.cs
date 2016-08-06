1c6dd109c672cf83272be8e58b3df0b3c2ad89ff
skyarkhangel <skyarkhangel@gmail.com>
2016-08-06 18:59:54 +0300

    Fix error with empty casings, incendiary shells. Add explode mote to projectile explosive.

--- Source/CombatRealism/Combat_Realism/Projectiles/ProjectileCR_Explosive.cs
+++ Source/CombatRealism/Combat_Realism/Projectiles/ProjectileCR_Explosive.cs
@@ -1,5 +1,6 @@
 using System;
 using Verse;
+using UnityEngine;
 
 namespace Combat_Realism
 {
@@ -53,11 +54,27 @@ namespace Combat_Realism
                 propsCR == null ? false : propsCR.damageAdjacentTiles,
                 preExplosionSpawnThingDef,
                 this.def.projectile.explosionSpawnChance);
+                ThrowBigExplode(base.Position.ToVector3Shifted() + Gen.RandomHorizontalVector(def.projectile.explosionRadius * 0.7f), def.projectile.explosionRadius * 0.6f);
             CompExplosiveCR comp = this.TryGetComp<CompExplosiveCR>();
             if (comp != null)
             {
                 comp.Explode(launcher);
             }
+
+        }
+
+        public static void ThrowBigExplode(Vector3 loc, float size)
+        {
+            if (!loc.ShouldSpawnMotesAt())
+            {
+                return;
+            }
+            MoteThrown moteThrown = (MoteThrown)ThingMaker.MakeThing(ThingDef.Named("Mote_BigExplode"), null);
+            moteThrown.ScaleUniform = Rand.Range(5f, 6f) * size;
+            moteThrown.exactRotationRate = Rand.Range(0f, 0f);
+            moteThrown.exactPosition = loc;
+            moteThrown.SetVelocityAngleSpeed((float)Rand.Range(6, 8), Rand.Range(0.002f, 0.003f));
+            GenSpawn.Spawn(moteThrown, loc.ToIntVec3());
         }
     }
 }
